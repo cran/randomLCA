19989200d0831108d7a1d70af2665c9e5234461b
Ken Beath <ken.beath@mq.edu.au>
2011-02-26 00:00:00 +0000

    version 0.7-4

--- R/misc.randomLCA.R
+++ R/misc.randomLCA.R
@@ -7,39 +7,17 @@ logLik.randomLCA <- function(object, ...)
     val
 }
 
-AIC.logLik <-
-  ## BIC for logLik objects
-  function(object, ...)
+BIC.randomLCA <-
+function (object, ...) 
 {
-  -2 * (c(object) - attr(object, "df"))
+    if (!is.element("randomLCA", class(object))) 
+        stop("Argument 'object' must be an object of class \"randomLCA\".")
+    BIC(logLik(object))
 }
-
-BIC.logLik <-
-  ## BIC for logLik objects
-  function(object, ...)
-{
-  -2 * (c(object) - attr(object, "df") * log(attr(object, "nobs"))/2)
-}
-
-
-#AIC <-
-#  ## Return the object's value of the Bayesian Information Criterion
-#  function(object, ...) UseMethod("BIC")
-#
-#BIC <-
-#  ## Return the object's value of the Bayesian Information Criterion
-#  function(object, ...) UseMethod("BIC")
 
 AIC.randomLCA <-
-  ## AIC for various fitted objects
-  function(object, ...)
-{
-	AIC(logLik(object))
-}
-
-BIC.randomLCA <-
-  ## BIC for various fitted objects
-  function(object, ...)
-{
-	BIC(logLik(object))
+function(object,...) {
+    if (!inherits(object, "randomLCA"))
+        stop("Use only with 'randomLCA' objects.\n")
+    AIC(logLik(object))
 }
